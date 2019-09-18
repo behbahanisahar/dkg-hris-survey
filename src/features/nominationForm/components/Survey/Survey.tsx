5b7e36465d1feef12a32741d2dc40bffffa9eabf
sahar behbahani <s.behbahani71@gmail.com>
2019-09-18 12:41:41 +0430

    change

--- src/features/nominationForm/components/Survey/Survey.tsx
+++ src/features/nominationForm/components/Survey/Survey.tsx
@@ -10,32 +10,20 @@ import {
   MDBCardText,
   MDBBtn,
   MDBRow,
-  MDBContainer
+  MDBContainer,
 } from "mdbreact";
 import "./../Survey/Survey.css";
 import ListServices from "../../../../services/list-services";
 import SPLists from "./../../../../entities/lists";
 import ReactSelect from "react-select";
 import Add from "@material-ui/icons/Add";
-import Delete from '@material-ui/icons/Delete';
-import {
-  Table,
-  TableHead,
-  TableRow,
-  TableBody,
-  TableCell,
-  Fab,
-  Card
-} from "@material-ui/core";
+import Delete from "@material-ui/icons/Delete";
+import { Table, TableHead, TableRow, TableBody, TableCell, Fab, Card } from "@material-ui/core";
 import ITableHeader from "../../../../entities/table-headers";
 import SnackBarMode from "../../../../entities/snackbar-mode";
 import SnackBarMessage from "../snakbar-message/snackbar-message";
 
-
-export default class Survey extends React.Component<
-  ISurveyProps,
-  ISurveyState
-> {
+export default class Survey extends React.Component<ISurveyProps, ISurveyState> {
   private ListService: ListServices;
   private tableHeaders: ITableHeader[];
   public constructor(props: ISurveyProps) {
@@ -44,7 +32,7 @@ export default class Survey extends React.Component<
     this.tableHeaders = [
       { id: "Row", label: "Row" },
       { id: "Selected", label: "Selected Person" },
-      { id: "Action", label: "Delete" }
+      { id: "Action", label: "Delete" },
     ];
     this.state = {
       UserInfo: [],
@@ -56,7 +44,7 @@ export default class Survey extends React.Component<
       rowsPerPage: 15,
       SelectedUsers: [],
       showSnackbarMessage: false,
-      snackbarMessage: '',
+      snackbarMessage: "",
       snackbarType: SnackBarMode.Info,
     };
   }
@@ -67,7 +55,7 @@ export default class Survey extends React.Component<
     this.setState(prevState => {
       return {
         ...prevState,
-        UserInfo
+        UserInfo,
       };
     });
     console.log(this.state.UserInfo);
@@ -78,18 +66,18 @@ export default class Survey extends React.Component<
       {
         label: "test1",
         value: 1,
-        icon: <Add />
+        icon: <Add />,
       },
       {
         label: "test2",
         value: 2,
-        icon: <Add />
+        icon: <Add />,
       },
       {
         label: "test3",
         value: 3,
-        icon: <Add />
-      }
+        icon: <Add />,
+      },
     ];
     const SelectedUsers = this.state.SelectedUsers;
     return (
@@ -109,10 +97,9 @@ export default class Survey extends React.Component<
                     <label htmlFor="formGroupExampleInput">Default input</label>
                   </MDBRow>
                   <MDBRow>
-                  <label htmlFor="formGroupExampleInput">Default input</label>
+                    <label htmlFor="formGroupExampleInput">Default input</label>
                   </MDBRow>
                   <MDBRow>
-                   
                     <ReactSelect
                       className="basic-single"
                       classNamePrefix="select"
@@ -122,55 +109,52 @@ export default class Survey extends React.Component<
                       isRtl={false}
                       isSearchable={true}
                       name="SelectedUser"
-                      onChange={(ev: any) =>
-                        this.onSelectAutoComplete(ev, "SelectedUser")
-                      }
+                      onChange={(ev: any) => this.onSelectAutoComplete(ev, "SelectedUser")}
                       options={option}
                       placeholder="select..."
                     />
                     <Fab size="small" color="primary" aria-label="add">
-                      <Add onClick={this.AddItem}/>
+                      <Add onClick={this.AddItem} />
                     </Fab>
                   </MDBRow>
-                 
+
                   <MDBRow>
                     <Card className="CardTable">
-                    <Table aria-labelledby="tableTitle">
-                      <TableHead>
-                        <TableRow>
-                          {this.renderHeader(this.tableHeaders)}
-                        </TableRow>
-                      </TableHead>
-                     
-                      <TableBody>
-          {SelectedUsers.map((n: any, index: any) => {
+                      <Table aria-labelledby="tableTitle">
+                        <TableHead>
+                          <TableRow>{this.renderHeader(this.tableHeaders)}</TableRow>
+                        </TableHead>
+
+                        <TableBody>
+                          {SelectedUsers.map((n: any, index: any) => {
                             return (
-            <TableRow key={index}>
-              <TableCell align="center">{index+1}</TableCell>
-              <TableCell align="center">{n}</TableCell>
-              <TableCell align="center"  onClick={() => this.DeleteItem(n)}><Delete/></TableCell>
-              
-            </TableRow>
-           );
-          })}
-        </TableBody>
-                    </Table>
+                              <TableRow key={index}>
+                                <TableCell align="center">{index + 1}</TableCell>
+                                <TableCell align="center">{n}</TableCell>
+                                <TableCell align="center" onClick={() => this.DeleteItem(n)}>
+                                  <Delete />
+                                </TableCell>
+                              </TableRow>
+                            );
+                          })}
+                        </TableBody>
+                      </Table>
                     </Card>
                   </MDBRow>
                 </MDBContainer>
               </MDBCardText>
-              <MDBBtn href="#" >
-                Save
-              </MDBBtn>
+              <MDBBtn href="#">Save</MDBBtn>
               <MDBBtn href="#">Cancel</MDBBtn>
             </MDBCardBody>
           </MDBCard>
         </MDBCol>
-        <SnackBarMessage type={this.state.snackbarType} message={this.state.snackbarMessage}
-                    showMessage={this.state.showSnackbarMessage}
-                    onHandleCloseMessage={this.handleCloseMessage} />
+        <SnackBarMessage
+          type={this.state.snackbarType}
+          message={this.state.snackbarMessage}
+          showMessage={this.state.showSnackbarMessage}
+          onHandleCloseMessage={this.handleCloseMessage}
+        />
       </div>
-      
     );
   }
   /*****************************select user**********************************************8 */
@@ -180,61 +164,53 @@ export default class Survey extends React.Component<
       return {
         ...prevState,
         [dropdownName]: event === null ? "" : event.label,
-        [dropdownId]: event === null ? 0 : event.value
+        [dropdownId]: event === null ? 0 : event.value,
       };
     });
     //this.state.SelectedUsers.push(this.state.SelectedUser);
   };
   /*********************************add item to table****************************************************** */
   private AddItem = () => {
-  
     const NewItem: any[] = this.state.SelectedUsers;
     if (NewItem.indexOf(this.state.SelectedUser) > -1) {
-      this.setState((prevState) => {
+      this.setState(prevState => {
         return {
-            ...prevState,
-            snackbarMessage: 'User Exist!',
-            showSnackbarMessage: true,
-            snackbarType: SnackBarMode.Error,
+          ...prevState,
+          snackbarMessage: "User Exist!",
+          showSnackbarMessage: true,
+          snackbarType: SnackBarMode.Error,
         };
-    });
-  } else {
-    NewItem.push(this.state.SelectedUser);
-    this.setState(prevState => {
-      return {
-        ...prevState,
-        SelectedUsers: NewItem
-      };
-    });
-  }
-   
+      });
+    } else {
+      NewItem.push(this.state.SelectedUser);
+      this.setState(prevState => {
+        return {
+          ...prevState,
+          SelectedUsers: NewItem,
+        };
+      });
+    }
   };
   /******************************delete item from table***************************************************** */
-  private DeleteItem = (currentItem:string) => {
-    this.setState((prevState) => {
+  private DeleteItem = (currentItem: string) => {
+    this.setState(prevState => {
       const prevValues = prevState.SelectedUsers || [];
-      const newValue= prevValues.filter(el => el !== currentItem);
-      return ({
-          ...prevState,
-          SelectedUsers: newValue,
-      });
-  });
-
-  }
+      const newValue = prevValues.filter(el => el !== currentItem);
+      return {
+        ...prevState,
+        SelectedUsers: newValue,
+      };
+    });
+  };
   /******************************render table header******************************************************** */
   private renderHeader = (columnDetail: any[]) => {
     return columnDetail.map(
       row => (
-        <TableCell
-          key={row.id}
-          align="center"
-          padding="none"
-          sortDirection="desc"
-        >
+        <TableCell key={row.id} align="center" padding="none" sortDirection="desc">
           {row.label}
         </TableCell>
       ),
-      this
+      this,
     );
   };
   /**************************sorting functions*********************************************** */
@@ -248,19 +224,17 @@ export default class Survey extends React.Component<
     return 0;
   }
 
-
-     /**************************** SnackBar ****************************** */
-     private handleCloseMessage = () => {
-      if (this.state.snackbarType === SnackBarMode.Success) {
-        alert("success");
-      }
-      else {
-          this.setState((prevState) => {
-              return ({
-                  ...prevState,
-                  showSnackbarMessage: false,
-              });
-          });
-      }
-  }
+  /**************************** SnackBar ****************************** */
+  private handleCloseMessage = () => {
+    if (this.state.snackbarType === SnackBarMode.Success) {
+      alert("success");
+    } else {
+      this.setState(prevState => {
+        return {
+          ...prevState,
+          showSnackbarMessage: false,
+        };
+      });
+    }
+  };
 }
