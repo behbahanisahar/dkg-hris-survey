cf31f7296d13b59222beaea4e363bc8504a92e9a
Ali Nooshabadi <ali.nooshabadi@gmail.com>
2019-09-23 12:11:22 +0330

    delete sp-rest-proxy folder

--- src/services/list-services.ts
+++ src/services/list-services.ts
@@ -1,61 +1,58 @@
-import { sp } from '@pnp/sp';
-import ServiceBase from './service-base';
-import MockData from './mock-data';
-import NominationData from '../entities/nomination';
-import IUpdatedData from '../entities/updatedNominationItem';
-import IHistory from '../entities/history';
-
-
+import { sp } from "@pnp/sp";
+import ServiceBase from "./service-base";
+import MockData from "./mock-data";
+import NominationData from "../entities/nomination";
+import IUpdatedData from "../entities/updatedNominationItem";
+import IHistory from "../entities/history";
 
 class ListServices extends ServiceBase {
-    public constructor() { 
-        super();
+  public constructor() {
+    super();
+  }
+
+  public async getUserInfo(listName: string): Promise<any> {
+    if (process.env.NODE_ENV === "production") {
+      const result: any[] = await sp.web.lists
+        .getByTitle(listName)
+        .items.filter("JobStatus eq 'شاغل'")
+        .select("LatinFullName", "EmailAddress", "SPLatinFullName", "Department", "Id")
+        .get();
+      return Promise.resolve(
+        result.map(({ SPLatinFullName: label, Id, EmailAddress, SPLatinFullName, Department }) => {
+          return {
+            label,
+            value: String(Id),
+            Department,
+            SPLatinFullName,
+            EmailAddress,
+          };
+        }),
+      );
+    }
+    return Promise.resolve(MockData.getUserInfo);
+  }
+  /****************get nomination form data*****************************************************8 */
+  public async getNominationData(itemId: number): Promise<NominationData> {
+    if (process.env.NODE_ENV === "production") {
+      const items: any = await this.get("/survey/nomination?itemId=" + itemId + "");
+      return Promise.resolve(items.data);
+    }
+    return Promise.resolve(MockData.NominationData);
+  }
+  /**********************get nomination form history******************************************* */
+  public async getNominationHistory(itemId: number): Promise<IHistory[]> {
+    if (process.env.NODE_ENV === "production") {
+      const items: any = await this.get("/survey/nomination/history?itemId=" + itemId + "");
+      console.log(items);
+      return Promise.resolve(items.data);
     }
 
-    public async getUserInfo(listName: string): Promise<any> {
-        if (process.env.NODE_ENV === 'production') {
-            const result: any[] = await sp.web.lists.getByTitle(listName).items
-            .select('LatinFullName', 'EmailAddress', 'SPLatinFullName','Department','Id')
-            .top(4900)
-            .get();
-            return Promise.resolve(result.map(({ SPLatinFullName: label, Id,EmailAddress,SPLatinFullName,Department }) => {
-                return ({
-                    label,
-                    value: String(Id),
-                    Department,
-                    SPLatinFullName,
-                    EmailAddress
-                });
-            }));
-          
-           
-        }
-        return Promise.resolve(MockData.getUserInfo);
-
-    }
-    /****************get nomination form data*****************************************************8 */
-    public async getNominationData(itemId:number): Promise<NominationData> {
-        if (process.env.NODE_ENV === 'production') {
-        const items: any = await this.get("/survey/nomination?itemId="+itemId+"");
-        return Promise.resolve(items.data);
-        }
-        return Promise.resolve(MockData.NominationData);
-      }
-      /**********************get nomination form history******************************************* */
-      public async getNominationHistory(itemId:number): Promise<IHistory[]> {
-        if (process.env.NODE_ENV === 'production') {
-        const items: any = await this.get("/survey/nomination/history?itemId="+itemId+"");
-        console.log(items);
-        return Promise.resolve(items.data);
-        }
-       
-        return Promise.resolve(MockData.NominationHistory);
-      }
-      /*******************put nomination form data************************************************** */
-      public async updateNominationData(param:IUpdatedData):Promise<IUpdatedData>{                  
-        const items: any = await this.put("/survey/nomination",param);
-         return Promise.resolve(items.data);  
-         
-    }
+    return Promise.resolve(MockData.NominationHistory);
+  }
+  /*******************put nomination form data************************************************** */
+  public async updateNominationData(param: IUpdatedData): Promise<IUpdatedData> {
+    const items: any = await this.put("/survey/nomination", param);
+    return Promise.resolve(items.data);
+  }
 }
 export default ListServices;
