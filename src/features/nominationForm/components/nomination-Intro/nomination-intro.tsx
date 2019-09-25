06ac50bfc57dba7ab591e5aa5d47b3a6d0109c58
sahar behbahani <s.behbahani71@gmail.com>
2019-09-25 13:09:21 +0330

    fix errors

--- src/features/nominationForm/components/nomination-Intro/nomination-intro.tsx
+++ src/features/nominationForm/components/nomination-Intro/nomination-intro.tsx
@@ -1,13 +1,8 @@
 import React from "react";
 import INominationIntroState from "./nomination-intro-state";
 
-
 export default class NominationIntroPage extends React.Component<INominationIntroState, {}> {
   public render() {
-    return (
-     <div>test</div>
-    );
+    return <div>test</div>;
   }
- 
-
-
+}
--- src/features/survey-form/components/survey-intro/survey-intro.tsx
+++ src/features/survey-form/components/survey-intro/survey-intro.tsx
@@ -1,12 +1,8 @@
 import React from "react";
 import ISurveyIntroState from "./survey-intro-state";
 
-
-
 export default class NominationIntroPage extends React.Component<ISurveyIntroState, {}> {
   public render() {
-    return (
-     <div>test</div>
-    );
+    return <div>test</div>;
   }
- 
+}
