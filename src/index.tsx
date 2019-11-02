9e2cb05e34521a4f28dfe91a53b37565a127b9c9
Ali Nooshabadi <ali.nooshabadi@gmail.com>
2019-11-02 11:10:06 +0330

    remove font-awsome

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import "react-app-polyfill/ie9";
 import React from "react";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
