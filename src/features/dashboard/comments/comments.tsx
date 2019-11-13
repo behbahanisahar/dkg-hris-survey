4c5b6109ff274aee624b9bb25083d9dfaafc0115
Ali Nooshabadi <ali.nooshabadi@gmail.com>
2019-11-13 13:45:50 +0330

    comments loader

--- src/features/dashboard/comments/comments.tsx
+++ src/features/dashboard/comments/comments.tsx
@@ -5,6 +5,7 @@ import { DKPortlet } from "../../../core/components/portlet/portlet";
 import { IComment } from "../../../entities/reports/comments";
 import { Grid } from "@material-ui/core";
 import "./comments.css";
+import Spinner from "../../spinner/spinner";
 
 interface IProps {
   name?: string;
@@ -34,29 +35,33 @@ export default class Comments extends React.Component<IProps, IState> {
       this.setState(current => ({
         ...current,
         data: response,
-        isFetching: false,
+        isFetching: true,
       })),
     );
   }
   public render() {
     return (
       <>
-        {this.state.isFetching === true && <div>Loading...</div>}
         <Grid container spacing={3}>
           {this.state.data.map(comment => (
             <Grid item xs={4}>
               <DKPortlet title={comment.Title}>
-                <div className={"kt-widget2 " + comment.Value}>
-                  {comment.Comments.map(text => (
-                    <div className="kt-widget2__item ">
-                      <div className="kt-widget2__info">
-                        <a href="#" className="kt-widget2__title">
-                          {text}
-                        </a>
-                      </div>
+                {this.state.isFetching === true && <Spinner></Spinner>}
+                {this.state.isFetching === false && (
+                  <div className="kt-scroll" data-scroll="true" data-height="200" data-scrollbar-shown="true">
+                    <div className={"kt-widget2 " + comment.Value}>
+                      {comment.Comments.map(text => (
+                        <div className="kt-widget2__item ">
+                          <div className="kt-widget2__info">
+                            <a href="#" className="kt-widget2__title">
+                              {text}
+                            </a>
+                          </div>
+                        </div>
+                      ))}
                     </div>
-                  ))}
-                </div>
+                  </div>
+                )}
               </DKPortlet>
             </Grid>
           ))}
