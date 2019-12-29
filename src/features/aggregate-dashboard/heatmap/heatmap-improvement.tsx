1c7ddf6fc86bbba083bedbf63b852a09acb5079e
Ali Nooshabadi <ali.nooshabadi@gmail.com>
2019-12-29 09:37:59 +0330

    change heatmap styles

--- src/features/aggregate-dashboard/heatmap/heatmap-improvement.tsx
+++ src/features/aggregate-dashboard/heatmap/heatmap-improvement.tsx
@@ -1,19 +1,19 @@
 import * as React from "react";
 
 export interface Props extends React.HTMLAttributes<HTMLDivElement> {
-  value: number;
+  value: string;
 }
 
-export function HeataImprovement({ value = 0 }: Props) {
+export function HeataImprovement({ value = "0" }: Props) {
   return (
     <div className="text-center">
-      {value > 0 && (
+      {Number(value) > 0 && (
         <span className="dk-brand-text-green">
           <i className="la la-angle-up ml-2"></i>
           {value} %
         </span>
       )}
-      {value < 0 && (
+      {Number(value) < 0 && (
         <span className="dk-brand-text-red">
           <i className="la la-angle-down ml-2"></i>
           {value} %
